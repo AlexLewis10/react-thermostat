c067985dc76d3ac3eae8079dff0ab593be741c3c
Alex Lewis <alexlewis374@gmail.com>
2020-06-10 17:13:46 +0100

    Connect to weather api, add form to submit location

--- src/components/thermostat.js
+++ src/components/thermostat.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import axios from "axios"
 
 export class Thermostat extends Component {
   constructor () {
@@ -10,7 +11,9 @@ export class Thermostat extends Component {
       currentTemp: 20,
       powerSavingMode: true,
       maxTempOff: 32,
-      locationWeather: null
+      locationWeather: null,
+      apiData: null,
+      location: ''
     }
   }
   
@@ -49,39 +52,46 @@ export class Thermostat extends Component {
     this.setState({currentTemp: this.state.defaultTemp})
   }
   
-  getWeather = () => {
-    fetch("../public/weather.json")
-    .then(response => response.json())
-    .then(weather =>
+  myChangeHandler = (event) => {
+    this.setState({location: event.target.value})
+
+  }
+
+  handleSubmit= (event) => {
+    event.preventDefault()
+    console.log("hello")
+    axios.get('http://api.openweathermap.org/data/2.5/weather?q='+ this.state.location +'&appid=a3d9eb01d4de82b9b8d0849ef604dbed')
+    .then(response => {
       this.setState({
-        locationWeather: weather
+        apiData: response.data.weather[0].description
       })
-    )
-    console.log(this.locationWeather)
+    })
+    .catch(error => {
+      console.log(error)
+    })
   }
 
 
-  // $('#update-weather').click( function(e) {
-  //   e.preventDefault()
-  //   var location = $('#location').val()
-  //   $.get('http://api.openweathermap.org/data/2.5/weather?q=' + location + '&appid=a3d9eb01d4de82b9b8d0849ef604dbed', function(data) {
-  //   $('#london-weather').text(data.weather[0].description);
-    
-  //   })
-  // })
-
   render() {
-    return (
-      <div>
-        <h1 className="temp">{this.state.currentTemp}C</h1>
-        <button className="btn" type="button" id='tempDown' onClick= {this.changeTempDown}>Temp Down</button>
-        <button className="btn" type="button" id='tempUp' onClick= {this.changeTempUp}>Temp Up</button>
-        <button className="btn" type="button" id='psmOn' onClick= {this.powerSavingModeOn}>Power Saving Mode ON</button>
-        <button className="btn" type="button" id='psmOff' onClick= {this.powerSavingModeOff}>Power Saving OFF</button>
-        <button className="btn" type="button" id='default' onClick= {this.setToCurrentTemp}>Default Temp</button>
-        <button className="btn" type="button" onClick= {this.getWeather}>Get Weather</button>
-      </div>
-    )
+      return (
+        <div>
+          <h1 className="temp">{this.state.currentTemp}C</h1>
+          <button className="btn" type="button" id='tempDown' onClick= {this.changeTempDown}>Temp Down</button>
+          <button className="btn" type="button" id='tempUp' onClick= {this.changeTempUp}>Temp Up</button>
+          <button className="btn" type="button" id='psmOn' onClick= {this.powerSavingModeOn}>Power Saving Mode ON</button>
+          <button className="btn" type="button" id='psmOff' onClick= {this.powerSavingModeOff}>Power Saving OFF</button>
+          <button className="btn" type="button" id='default' onClick= {this.setToCurrentTemp}>Default Temp</button>
+          <form onSubmit={this.handleSubmit}>
+            <p>Enter City</p>
+            <input 
+              type='text'
+              onChange={this.myChangeHandler}
+              />
+              <input type='submit' />
+          </form>
+          <p>{this.state.apiData}</p>
+        </div>
+      )
   }
 }
 
